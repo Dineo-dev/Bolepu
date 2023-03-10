50c002e7f798b148610dfbbe93c8c14ee9b4d148
Dineo <dineo.phahla@dadevs.co.za>
2023-03-10 15:10:09 +0200

    css

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Services from './components/services/Services'
 import Testimonials from './components/testimonials/Testimonials'
 import Footer from './components/footer/Footer'
 import Contact from './components/contact/Contact'
+import './index.css'
 
 const App = () => {
   return (
