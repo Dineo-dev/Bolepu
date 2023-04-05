ae258e9aef3f27a9af4d4adce1636ee2ccf4ece7
Dineo <dineo.phahla@dadevs.co.za>
2023-04-05 09:46:08 +0200

    Name not displayed

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,8 +8,8 @@ const Header = () => {
       <div className='container header__container'> 
       
       <h5> Hello I'm</h5>
-      <h1> Dineo Phahla</h1>
-      <h5 className='text-light'> Fullstack Developer </h5>
+      {/* <h1> Dineo Phahla</h1>
+      <h5 className='text-light'> Fullstack Developer </h5> */}
       
       </div>
     </Header>
